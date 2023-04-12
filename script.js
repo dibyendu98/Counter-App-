f5fc66e826d58785b755a1359715ecf7f34e3f2c
dibyendu <dibyenduchattoraj1998@gmail>
2023-04-12 08:18:12 +0530

    Files added

---
+++ script.js
@@ -0,0 +1,20 @@
+var x=0;
+
+function plus()
+{
+   x++;
+   counter.innerHTML=x;
+}
+
+function minus()
+{
+    x--;
+    counter.innerHTML=x;
+}
+
+function reset()
+{
+    x=0;
+    counter.innerHTML=x;
+}
+
